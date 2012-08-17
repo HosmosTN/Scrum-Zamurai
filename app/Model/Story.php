e98f5e416316cd01c28122a3d22edff9d091c2ce
SENDA Keijiro <senda.keijiro@gmail.com>
2012-08-17 11:34:06 +0900

    Added models - Sprint and Story.

---
+++ app/Model/Story.php
@@ -0,0 +1,54 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Story Model
+ *
+ * @property Sprint $Sprint
+ */
+class Story extends AppModel {
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'description' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'status' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'Sprint' => array(
+			'className' => 'Sprint',
+			'foreignKey' => 'sprint_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+}
