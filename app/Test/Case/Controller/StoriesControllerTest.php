ab0dee59cda7871fa05a31574dc588fabb67afdb
SENDA Keijiro <senda.keijiro@gmail.com>
2012-10-12 16:22:03 +0900

    Added StoriesControllerTest.

---
+++ app/Test/Case/Controller/StoriesControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+App::uses('StoriesController', 'Controller');
+
+/**
+ * StoriesController Test Case
+ *
+ */
+class StoriesControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.story',
+		'app.sprint'
+	);
+
+/**
+ * testIndex method
+ *
+ * @return void
+ */
+	public function testIndex() {
+        $this->markTestIncomplete();
+	}
+
+/**
+ * testAdd method
+ *
+ * @return void
+ */
+	public function testAdd() {
+        $this->markTestIncomplete();
+	}
+
+/**
+ * testEdit method
+ *
+ * @return void
+ */
+	public function testEdit() {
+        $this->markTestIncomplete();
+	}
+
+}
