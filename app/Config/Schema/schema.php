dd65606bd0ca4e278894d42b7be77e14d0a58475
SENDA Keijiro <senda.keijiro@gmail.com>
2012-12-19 16:24:16 +0900

    Added 'date_from' and 'date_to' columns to Sprint table.

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -15,6 +15,8 @@ class AppSchema extends CakeSchema {
 		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'description' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'status' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 45, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'date_from' => array('type' => 'date', 'null' => false, 'default' => null),
+		'date_to' => array('type' => 'date', 'null' => false, 'default' => null),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1)
 		),
@@ -27,7 +29,7 @@ class AppSchema extends CakeSchema {
 		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'description' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'status' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 45, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'priority' => array('type' => 'boolean', 'null' => true, 'default' => null, 'comment' => '0 = p0, 1 = p1, ..., 4 = p4'),
+		'priority' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 2, 'collate' => 'utf8_general_ci', 'comment' => 'p0, p1, ..., p4', 'charset' => 'utf8'),
 		'estimate' => array('type' => 'integer', 'null' => true, 'default' => null),
 		'record' => array('type' => 'integer', 'null' => true, 'default' => null),
 		'parent_id' => array('type' => 'integer', 'null' => true, 'default' => null),
